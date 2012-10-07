5c6da22f5ecd415a38e46db8dd2164c2ffad411e
lbo <lbo@spheniscida.de>
2012-10-07 10:10:53 +0200

    Init

---
+++ client.c
@@ -0,0 +1,81 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <libsocket/libinetsocket.h>
+# include <sys/select.h>
+# include <string.h>
+# include <termios.h>
+
+struct termios original_state;
+
+void set_tty_raw(void);
+void reset_tty(void);
+
+int main(void)
+{
+	int clfd = 0;
+	fd_set fds;
+	ssize_t read_bytes = 1;
+	char buffer[32];
+
+	clfd = create_inet_stream_socket("192.168.1.8","12345",IPv4,0);
+
+	// Get all characters from calling tty (client tty)
+	set_tty_raw();
+
+	// Route data between user and server
+	while ( read_bytes > 0 )
+	{
+		FD_ZERO(&fds);
+		FD_SET(clfd,&fds);
+		FD_SET(0,&fds);
+
+		memset(buffer,0,32);
+
+		select(clfd+1,&fds,NULL,NULL,NULL);
+
+		if ( FD_ISSET(clfd,&fds) )
+		{
+			read_bytes = read(clfd,buffer,31);
+			write(1,buffer,read_bytes);
+		}
+
+		if ( FD_ISSET(0,&fds) )
+		{
+			read_bytes = read(0,buffer,31);
+			write(clfd,buffer,read_bytes);
+		}
+	}
+
+	reset_tty();
+
+	close(clfd);
+
+	return 0;
+}
+
+// Black terminal magic...
+
+void set_tty_raw(void)
+{
+	struct termios attrs;
+
+	tcgetattr(0,&original_state);
+	tcgetattr(0,&attrs);
+
+	attrs.c_lflag &= ~(ICANON|ISIG|IEXTEN|ECHO);
+
+	attrs.c_iflag &= ~(BRKINT|ICRNL|IGNBRK|IGNCR|INLCR|INPCK|ISTRIP|IXON|PARMRK);
+
+	attrs.c_oflag &= ~OPOST;
+
+	attrs.c_cc[VMIN] = 1;
+	attrs.c_cc[VTIME] = 0;
+
+	tcsetattr(0,TCSAFLUSH,&attrs);
+}
+
+void reset_tty(void)
+{
+	tcsetattr(0,TCSANOW,&original_state);
+}
---
+++ termserv.c
@@ -0,0 +1,179 @@
+# define _GNU_SOURCE
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <termios.h>
+# include <string.h>
+# include <sys/ioctl.h>
+# include <libsocket/libinetsocket.h>
+# include <termios.h>
+# include <sys/wait.h>
+
+struct termios original_state;
+
+pid_t cpid;
+
+void fork_child_shell(const char*);
+void set_tty_raw(void);
+void reset_tty(void);
+
+int main(int argc, char** argv)
+{
+	int masterfd = 0, selret = 0, scriptfd = 0, retbuf = 0, server = 0, accepted = 0;
+	ssize_t read_bytes = 0;
+	char* slave_pts;
+	char buffer[32];
+
+	fd_set fds;
+
+	// Allocate new pseudo terminal
+	masterfd = posix_openpt(O_RDWR|O_NOCTTY);
+	
+	if ( masterfd < 0 )
+	{
+		perror("posix_openpt\n");
+		exit(1);
+	}
+
+	// Change permissions of terminal
+	retbuf = grantpt(masterfd);
+	
+	if ( retbuf < 0 )
+	{
+		perror("grantpt\n");
+		exit(1);
+	}
+
+	// Get name of terminal (/dev/pts/...)
+	slave_pts = ptsname(masterfd);
+
+	if ( slave_pts == NULL )
+	{
+		perror("ptsname\n");
+		exit(1);
+	}
+
+	// Unlock terminal so the child can read/write from/to it
+	retbuf = unlockpt(masterfd);
+	
+	if ( retbuf < 0 )
+	{
+		perror("unlockpt\n");
+		exit(1);
+	}
+
+	// Get TCP connection
+	
+	server = create_inet_server_socket("0.0.0.0","12345",TCP,IPv4,0);
+	
+	accepted = accept_inet_stream_socket(server,NULL,0,NULL,0,0,0);
+
+	close(server);
+	
+	// Fork child
+	fork_child_shell(slave_pts);
+
+	// Set raw mode for current tty (noecho, no special character interpretation...)
+	//set_tty_raw();
+
+	read_bytes = 1;
+
+	// Get input from user/output from terminal
+	while ( read_bytes > 0 )
+	{
+		FD_ZERO(&fds);
+		FD_SET(accepted,&fds);
+		FD_SET(masterfd,&fds);
+
+		selret = select(accepted > masterfd ? accepted+1 : masterfd+1,&fds,NULL,NULL,NULL);
+
+		if ( selret == -1 )
+			perror("select\n");
+
+		if ( FD_ISSET(accepted,&fds) ) // We may read data from the network connection
+		{
+			memset(buffer,0,32);
+			
+			read_bytes = read(accepted,buffer,31);
+			write(masterfd,buffer,read_bytes);
+
+			continue;
+		}
+
+		if ( FD_ISSET(masterfd,&fds) ) // The child process wrote something to the terminal
+		{
+			memset(buffer,0,32);
+
+			read_bytes = read(masterfd,buffer,31);
+			write(accepted,buffer,read_bytes);
+
+			continue;
+		}
+	}
+	
+	// Wait for the shell
+	waitpid(cpid,0,0);
+
+	close(accepted);
+	close(masterfd);
+
+	return 0;
+}
+
+void fork_child_shell(const char* slavepts)
+{
+	int slavefd;
+	char* shell = getenv("SHELL");
+	struct winsize ws;
+
+	// Gain window size
+	ioctl(0,TIOCGWINSZ,&ws);
+
+	// Which shell do we execute?
+	if ( shell == NULL )
+		shell = "/bin/bash";
+	
+	cpid = fork();
+	
+	if ( cpid < 0 )
+	{
+		perror("fork\n");
+		exit(1);
+	}
+
+	// Parent...
+	if ( cpid != 0 )
+		return;
+
+	// Child...
+	if ( cpid == 0 ) // We're in the child process
+	{
+		// Open the slave part of the pseudo terminal
+		slavefd = open(slavepts,O_RDWR);
+		
+		// New session (enables job control in bash)
+		setsid();
+
+		// Set windows size obtained before
+		ioctl(slavefd,TIOCSWINSZ,&ws);
+
+		// Close original stdin, stdout, stderr
+		close(0);
+		close(1);
+		close(2);
+
+		// Duplicate the pseudo terminal to all file descriptors
+		dup2(slavefd,0);
+		dup2(slavefd,1);
+		dup2(slavefd,2);
+
+		// Finally, execute shell
+		execl(shell,shell,(char*)NULL);
+
+		perror("Couldn't exec\n");
+
+		exit(1);
+	}
+}
+
